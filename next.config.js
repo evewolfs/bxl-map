4da616a408af1d9785cce327aa768210686c0e76
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-03-27 18:55:04 +0100

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/bxl-map",
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
