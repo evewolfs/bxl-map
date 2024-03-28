2a7aa76a4d6984168f943c4336c4659e983d870a
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-03-28 21:41:24 +0100

    config2141

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
 const webpack = require('webpack');
 
 const nextConfig = {
-  assetPrefix: '/bxl-map/',
-   basePath: '/bxl-map/',
-   output: "export",  // <=== enables static exports
+  // assetPrefix: '/bxl-map/',
+  //  basePath: '/bxl-map/',
+  //  output: "export",  // <=== enables static exports
    reactStrictMode: true,
 };
 
-module.exports = {
-  webpack: (config) => {
-    // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your actual Mapbox access token
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN': JSON.stringify('pk.eyJ1IjoiZXZld29sZnMiLCJhIjoiY2t3ZTBjMW4wMDAzODJxcDJ2ZHNzaGN6dSJ9.TMIw2kv_p_oW2oosW1LY2w')
-      })
-    );
+// module.exports = {
+//   webpack: (config) => {
+//     // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your actual Mapbox access token
+//     config.plugins.push(
+//       new webpack.DefinePlugin({
+//         'process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN': JSON.stringify('pk.eyJ1IjoiZXZld29sZnMiLCJhIjoiY2t3ZTBjMW4wMDAzODJxcDJ2ZHNzaGN6dSJ9.TMIw2kv_p_oW2oosW1LY2w')
+//       })
+//     );
 
-    return config;
-  },
+//     return config;
+//   },
 
   
-};
+// };
