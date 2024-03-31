2d5b537732cbb1973c1c24930ebd53143336ac61
waleedkhano <khanow99@gmail.com>
2024-03-31 23:39:57 +0500

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: "/bxl-map",
+//   assetPrefix: "/bxl-map/",
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+
+const webpack = require('webpack');
+
 const nextConfig = {
   basePath: "/bxl-map",
-  assetPrefix: "/bxl-map/",
+  output: "export", 
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your actual Mapbox access token
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN': JSON.stringify('pk.eyJ1IjoiZXZld29sZnMiLCJhIjoiY2t3ZTBjMW4wMDAzODJxcDJ2ZHNzaGN6dSJ9.TMIw2kv_p_oW2oosW1LY2w')
+      })
+    );
+    return config;
+  }
+};
