98783616d6ddb4c22b13e391562d2022770245a0
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-04-01 19:53:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/bxl-map",
-  assetPrefix: "/bxl-map",
   reactStrictMode: true,
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
